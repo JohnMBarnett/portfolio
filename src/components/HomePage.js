19fbc5eff920a3280c843cc696f8110c80f6d0eb
onuri5 <jomibaofficial@gmail.com>
2022-04-07 21:41:50 -0500

    created navbar

---
+++ src/components/HomePage.js
@@ -0,0 +1,11 @@
+import NavBar from "./NavBar";
+
+const HomePage = () => {
+  return (
+    <div className="home_container">
+      <NavBar />
+    </div>
+  )
+}
+
+export default HomePage;
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <h1>
+        JB.
+      </h1>
+      <div>
+        <a>About</a>
+        <em>|</em>
+        <a>Projects</a>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar;
